501b2da901d595d68673d527d1408229e7338a4a
hanijack <hani.darklt@gmail.com>
2023-01-17 17:45:36 +0200

    Addition to the contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,158 +1,55 @@
-// import React from 'react';
-// import { Context } from '../components/Context'
+import { Context } from "../components/Context";
+import { useContext ,useState} from 'react';
+
+const Contact = () => {
+    const { form, setForm } = useState({
+        email: " ",
+        password: " ",
+        textArea: " ",
+    });
+    function toggleForm(e) {
+        let { name, value } = e.target;
+        setForm((prev) => ({
+        ...prev,
+        [name]: value,
+        }));
+    }
+    function submitForm(e) {
+        e.preventDefault();
+    }
+    const { theme } =useContext(Context);
+    return (
+        <div className={`contact-container ${theme}`}>
+            <form action="https://formsubmit.co/hani.darklt@gmail.com" method="Post">
+                <input
+                    type="text"
+                    name="email"
+                    value={form.email}
+                    onChange={(e) => toggleForm(e)}
+                    required
+                />
+                <input
+                    type="password"
+                    name="password"
+                    value={form.password}
+                    onChange={(e) => toggleForm(e)}
+                    required
+                />
+                <input
+                    type="text"
+                    name="textArea"
+                    value={form.textArea}
+                    onChange={(e) => toggleForm(e)}
+                />
+                <button onClick={(e) =>{submitForm(e)}}>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default Contact;
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from "react"
-
-// export default function App() {
-//     const [formData, setFormData] = React.useState({
-//         email: "",
-//         password: "",
-//         passwordConfirm: "",
-//         joinedNewsletter: true
-//     })
-    
-    /**
-     * Challenge: Connect the form to local state
-     * 
-     * 1. Create a state object to store the 4 values we need to save.
-     * 2. Create a single handleChange function that can
-     *    manage the state of all the inputs and set it up
-     *    correctly
-     * 3. When the user clicks "Sign up", check if the 
-     *    password & confirmation match each other. If
-     *    so, log "Successfully signed up" to the console.
-     *    If not, log "passwords do not match" to the console.
-     * 4. Also when submitting the form, if the person checked
-     *    the "newsletter" checkbox, log "Thanks for signing
-     *    up for our newsletter!" to the console.
-     */
-    
-//     function handleChange(event) {
-//         const {name, value, type, checked} = event.target
-//         setFormData(prevFormData => ({
-//             ...prevFormData,
-//             [name]: type === "checkbox" ? checked : value
-//         }))
-//     }
-    
-//     function handleSubmit(event) {
-//         event.preventDefault()
-//         if(formData.password === formData.passwordConfirm) {
-//             console.log("Successfully signed up")
-//         } else {
-//             console.log("Passwords do not match")
-//             return
-//         }
-        
-//         if(formData.joinedNewsletter) {
-//             console.log("Thanks for signing up for our newsletter!")
-//         }
-//     }
-    
-//     return (
-//         <div className="form-container">
-//             <form className="form" onSubmit={handleSubmit}>
-//                 <input 
-//                     type="email" 
-//                     placeholder="Email address"
-//                     className="form--input"
-//                     name="email"
-//                     onChange={handleChange}
-//                     value={formData.email}
-//                 />
-//                 <input 
-//                     type="password" 
-//                     placeholder="Password"
-//                     className="form--input"
-//                     name="password"
-//                     onChange={handleChange}
-//                     value={formData.password}
-//                 />
-//                 <input 
-//                     type="password" 
-//                     placeholder="Confirm password"
-//                     className="form--input"
-//                     name="passwordConfirm"
-//                     onChange={handleChange}
-//                     value={formData.passwordConfirm}
-//                 />
-                
-//                 <div className="form--marketing">
-//                     <input
-//                         id="okayToEmail"
-//                         type="checkbox"
-//                         name="joinedNewsletter"
-//                         onChange={handleChange}
-//                         checked={formData.joinedNewsletter}
-//                     />
-//                     <label htmlFor="okayToEmail">I want to join the newsletter</label>
-//                 </div>
-//                 <button 
-//                     className="form--submit"
-//                 >
-//                     Sign up
-//                 </button>
-//             </form>
-//         </div>
-//     )
-// }
-
-
-// const Contact = () => {
-//   const {form , setForm}=React.useState({
-//     email:"",
-//     password:"",
-//     textArea:"",
-//   })
-//   function toggleForm(e){
-//     let {name , value}=e.target
-//     setForm(prev =>({
-//       ...prev,
-//       [name]: value
-//     }))
-//   }
-//   function submitForm(e){
-//     e.preventDefault()
-//   }
-//     const {theme}=React.useContext(Context)
-//   return (
-//     <div className={`contact-container ${theme}`}>
-//       Contact
-//       <input type="text" value={form.email}  name="email" 
-//       onChange={(e)=>toggleForm(e)}/>
-//       <input type="password" value={form.password} name="password" 
-//       onChange={(e)=>toggleForm(e)}/>
-//       <input type="text" value={form.textArea} name="textArea" 
-//       onChange={(e)=>toggleForm(e)}/>
-      
-      
-//       </div>
-//   )
-// }
-
-// export default Contact
